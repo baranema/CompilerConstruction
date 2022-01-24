8648feefa061b3f53b0631a132aee76662b1e55b
ebaranauskaite <e.b.baranauskaitema@student.vu.nl>
2022-01-24 13:00:16 +0200

    cnstprop

--- llvm-passes/constprop.cpp
+++ llvm-passes/constprop.cpp
@@ -10,74 +10,82 @@ namespace {
     };
 }
 
-bool calculate(int operand, int rhs, int lhs, int &result) {
-    switch(operand) {
-        case Instruction::Add:
-            result = lhs + rhs;
-            break;
-        case Instruction::Sub:
-            result = lhs - rhs;
-            break;
-        case Instruction::Mul:
-            result = lhs * rhs;
-            break;
-        case Instruction::AShr:
-            result = lhs >> rhs;
-            break;
-        case Instruction::Shl:
-            result = lhs << rhs;
-            break;
-        default:
-            return false;
-            break;
+bool calculate(int operand, int rightside, int leftside, int &result) {
+    // operations that can be simplified (example: additions where one operand is a 0)
+    if (rightside == 0 && (operand == Instruction::Sub || operand == Instruction::Shl ||
+        operand == Instruction::AShr || operand == Instruction::Add)) {
+        result = leftside; 
     } 
+    else if (leftside == 0 && operand == Instruction::Add) {
+        result = rightside;
+    } 
+    else if (rightside == 1 && operand == Instruction::Mul) {
+        result = leftside;
+    } 
+    else if (leftside == 1 && operand == Instruction::Mul) {
+        result = rightside;
+    } 
+    else if ((rightside == 0 && operand == Instruction::Mul) || (rightside == 0 && operand == Instruction::AShr)) {
+        result = rightside;
+    } 
+    else if ((leftside == 0 && operand == Instruction::Mul) || (leftside == 0 && operand == Instruction::Shl)) {
+        result = leftside;
+    } else {
+        switch (operand) {
+            case Instruction::Add:
+                result = leftside + rightside;
+                break;
+            case Instruction::Sub:
+                result = leftside - rightside;
+                break;
+            case Instruction::Mul:
+                result = leftside * rightside;
+                break;
+            case Instruction::AShr:
+                result = leftside >> rightside;
+                break;
+            case Instruction::Shl:
+                result = leftside << rightside;
+                break;
+            default:
+                return false;
+                break;
+        } 
+    }
     return true;
 }
 
-
 bool ConstProp::runOnFunction(Function &F) {
-    SmallVector<ConstantInt*, 2> consts;
-    SmallVector<Instruction*, 2> insts; 
-    
+    SmallVector<ConstantInt*, 2> consts; 
     bool IRchanged = false;
+
     for (BasicBlock &BB : F) {
         for (Instruction &I : BB) {
-            if (dyn_cast<BinaryOperator>(&I)) {  
-                for (Use &op : I.operands()) {
-                    if (dyn_cast<ConstantInt>(op)) {
+            if (dyn_cast<BinaryOperator>(&I)) { // if instruction is binary operation 
+                for (Use &op : I.operands()) { 
+                    if (dyn_cast<ConstantInt>(op)) { // is operand is a constant
                         consts.push_back(dyn_cast<ConstantInt>(op)); 
-                    } else if (dyn_cast<Instruction>(op)){
-                        insts.push_back(dyn_cast<Instruction>(op));
-                    } 
+                    }  
                 }
-
+ 
                 if (consts.size() == 2) {
                     int rightside = consts[consts.size() - 1]->getSExtValue();
                     int leftside = consts[consts.size() - 2]->getSExtValue();
-                    int result;
+                    int calculatedResult;
+                    ConstantInt *retrievedConstInt = nullptr;
 
-                    if (calculate(I.getOpcode(), rightside, leftside, result)) {
-                        ConstantInt *retrievedConstInt = ConstantInt::get(consts.back()->getType(), result);
-                        consts.pop_back(); 
-
+                //    LOG_LINE(leftside << " " << rightside);
+ 
+                    if (calculate(I.getOpcode(), rightside, leftside, calculatedResult)) {
+                        retrievedConstInt = ConstantInt::get(consts.back()->getType(), calculatedResult);
+                        consts.pop_back();
                         if (dyn_cast<Value>(retrievedConstInt)) { 
                             I.replaceAllUsesWith(retrievedConstInt);
                             IRchanged = true;
                         } 
                     }
-                } else if (consts.size() == 2 && insts.size() == 1) { 
-                    int op = I.getOpcode();
-                    
-                    if ((op == Instruction::Add && consts.back()->getSExtValue() == 0) ||  
-                        (op == Instruction::Sub && consts.back()->getSExtValue() == 0) ||
-                        (op == Instruction::Mul && consts.back()->getSExtValue() == 1)) {
-                        I.replaceAllUsesWith(insts.back()); 
-                        insts.pop_back();
-                    }
-                    consts.pop_back(); 
-                } 
-                consts.clear();
-                insts.clear();
+                }
+                consts.clear(); 
             }
         }
     }
